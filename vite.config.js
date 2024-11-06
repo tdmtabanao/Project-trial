7af23f638ef3cf41da20407dd5f742470e86b9e1
Tan <68370190+tdmtabanao@users.noreply.github.com>
2024-11-06 23:13:09 +0800

    base url

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/project-trial/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
